bb616fd22e0455e0af834600723fe3422ebcad47
Adesipe Adeola <princeadeola234@gmail.com>
2022-08-20 05:13:04 -0700

    Initial commit

---
+++ src/step1.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        double conversionRate = 650; //using dollar first
+        double userAmount = 500;
+
+        double converted = userAmount * conversionRate;
+        System.out.println(converted);
+    }
+}
+//Building a console application
+//Create my java project
+//Create a variable to store a hard-coded currency rate and input
+//Complete the app by implementing the whole conversion
+//Enhance the class by creating a method to convert the currency
+//Enhance the class to get the amount to be converted from the user
+//Enhance the class to get the conversion rate from the user
+//Enhance the class to get the conversion rate online
+//Enhance the application to give users the option of online or manual conversion rate
+//Make it more friendly and fun
+//Push final code to GitHub
